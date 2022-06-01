3e84b39d1ab2425edcc47b48e226fff52407a607
Ubuntu <ubuntu@ip-172-31-23-169.ca-central-1.compute.internal>
2022-06-01 22:44:18 +0000

    make for tensorgen on 12heatmap

--- datagen.cpp
+++ datagen.cpp
@@ -495,7 +495,7 @@ void Datagen::Solve() {
 
         // save kSupPath Collision to k seperate tensor files
         // vector<vector<Map2d<int>>> kTimeCollision_tmp(kTimeCollision);
-        for (int k = 0; k < _kmax; ++k){
+        for (int k = 1; k < _kmax; ++k){
             // Map2d<int> kCollision(_height, vector<int>(_width, 0));
             // for (int t = 0; t < static_cast<int>(max_kSubPath_size); ++t){
             //     ReduceMap(kTimeCollision_tmp[k][t], kTimeCollision_tmp[k][t], &myMinusOneOnPositive);
@@ -581,7 +581,7 @@ int main(int argc, char* argv[]) {
     cout << "Load()" << endl;
 
     datagen.Load(datagen._scenfile);
-    datagen._kmax = 1;
+    datagen._kmax = 3;
     cout << "Solve()" << endl;
     datagen.Solve();
 }
